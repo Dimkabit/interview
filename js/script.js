5728b5b1e269be73bd27f6b6fb1b54d3ce8a5c10
Dmitry <65062995+Dimkabit@users.noreply.github.com>
2021-09-10 20:10:04 +0200

    add new block

--- js/script.js
+++ js/script.js
@@ -14,3 +14,5 @@ headerBerger.addEventListener('click', (e) => {
 });
 
 
+
+
---
+++ js/video.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+	$('#app').vide({
+		mp4: 'video/SAINT JAMES - Collection Printemps_Été 2019.mp4',
+		webm: 'video/SAINT JAMES - Collection Printemps_Été 2019 (convert-video-online.com).webm',
+		ogv: 'video/SAINT JAMES - Collection Printemps_Été 2019.ogv',
+		poster: '../img/image2.png',
+		 
+   }, {
+		autoplay: false,
+	});
+});
+
+
+const playBtn = document.getElementById('play');
+const muteBtn = document.getElementById('mute')
+playBtn.addEventListener('click', (e) => {
+	e.preventDefault();
+	
+	if(playBtn) {
+		$(document).ready(function () {
+			$('#app').vide({
+				mp4: 'video/SAINT JAMES - Collection Printemps_Été 2019.mp4',
+				webm: 'video/video.webm',
+				ogv: 'video/video.ogv',
+				poster: '../img/image2.png',
+				 
+			}, {
+				autoplay: true,
+				muted: false,
+			});
+		});
+	} else {
+		$(document).ready(function () {
+			$('#app').vide({
+				mp4: 'video/SAINT JAMES - Collection Printemps_Été 2019.mp4',
+				webm: 'video/video.webm',
+				ogv: 'video/video.ogv',
+				poster: '../img/image2.png',
+				 
+			}, {
+				autoplay: false,
+			});
+		});
+	}
+});
+
+muteBtn.addEventListener('click', (e) => {
+	e.preventDefault();
+	if(muteBtn) {
+		$(document).ready(function () {
+			$('#app').vide({
+				mp4: 'video/SAINT JAMES - Collection Printemps_Été 2019.mp4',
+				webm: 'video/video.webm',
+				ogv: 'video/video.ogv',
+				poster: '../img/image2.png',
+				 
+			}, {
+				muted: true,
+			});
+		});
+	}
+})
+
+
